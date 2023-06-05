27fbf1c2879e5e222e47e31af5a8f2ccbdb09450
Pavel <pps.mayday@gmail.com>
2023-06-05 14:01:30 +0400

    Update navbar(front)

--- source/frontend/src/App.js
+++ source/frontend/src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route, Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Navbar';
 
 import Billboard from './components/billboard';
 import Profile from './components/profile';
@@ -11,18 +12,28 @@ import Login from './components/login';
 import Signup from './components/signup';
 
 function App() {
+  const user = null;
   return (
     <div className="App">
-    <Navbar bg='primary' variant='dark'>
-    <div className='container-fluid'>
-    <Navbar.Brand>React-bootstrap</Navbar.Brand>
-    <Nav className='me-auto'>
-    <Nav.Link href='#home'>Home</Nav.Link>
-    <Nav.Link href='#link'>Link</Nav.Link>
-    </Nav>
-    </div>
-    </Navbar>
-      Hello world
+        <Navbar className='navbar-bg' variant='dark'>
+            <div className='container-fluid'>
+                <Navbar.Brand>DevFuse</Navbar.Brand>
+                    <Nav className='me-auto'>
+                        <Container>
+                            <Link class='nav-link' to={"/api/billboard"}>Доска объявлений</Link>
+                            { true ? (
+                            <Link class='nav-link'>Logout ({user})</Link>
+                            ) : (
+                            <>
+                                <Link class='nav-link' to={'/login'}>Login</Link>
+                                <Link class='nav-link' to={'/signup'}>Signup</Link>
+                            </>
+                            )}
+                        </Container>
+                    </Nav>
+            </div>
+        </Navbar>
+        Hello world
     </div>
   );
 }
